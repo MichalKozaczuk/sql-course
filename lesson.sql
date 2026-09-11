e0fd8dd9600dee3cf5d9a0ae4554d07ebc0007f2
MichalKozaczuk <Michal.Kozaczuk@somersetft.nhs.uk>
2026-09-11 12:59:53 +0000

    Basic statememnts - SELECT, WHERE, AND, OR

--- lesson.sql
+++ lesson.sql
@@ -1,8 +1,14 @@
 /*
 Example lesson stub
 */
-
-SELECT
-	*
-FROM
-	PatientStay ps ;
+SELECT ps.PatientId,
+       ps.Hospital,
+       ps.Ward,
+       ps.AdmittedDate,
+       ps.DischargeDate,
+       ps.Ethnicity
+FROM   PatientStay AS ps
+WHERE  ps.Hospital IN ('Kingston', 'PRUH') -- when using IN () this creates list related to that selection code
+       AND (ps.Ward LIKE '%Surgery'
+            OR ps.Ward LIKE '%o%')
+       AND ps.Ethnicity IS NULL
